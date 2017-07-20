bcaabe9b3d6dc437aa458507289ac02ba4038b27
ljnicol <l.nicol.9@gmail.com>
2017-07-20 16:25:10 +1000

    Update to use 2016 census and short headers

--- src/create_database.py
+++ src/create_database.py
@@ -6,6 +6,7 @@ from sqlalchemy import Table, Column, Integer, String, MetaData
 import progressbar
 import argparse
 import re
+import xlrd
 
 def main():
   parser = argparse.ArgumentParser(description="Create a dataset from an existing database of tablebuilder data.")
@@ -17,7 +18,7 @@ def main():
   # 'sqlite:///../data/2011_BCP_ALL_for_AUST_long-header.db'
   connection_string = 'sqlite:///'+args.database
   disk_engine = create_engine(connection_string) # Initializes database
-  geo_levels_to_read = ['sa1', 'sa2', 'sa3', 'sa4']
+  geo_levels_to_read = ['sa4']
   read_data_for_geo_level_into_database(directory, geo_levels_to_read, disk_engine)
 
 def get_table_names_from_database(disk_engine):
@@ -29,9 +30,9 @@ def get_column_names_from_table(disk_engine, table_name):
   columns = insp.get_columns(table_name)
   return map(lambda column: column['name'], columns)
 
-def get_column_list_table_name_array(table_name, column_list):
+def get_column_list_table_name_array(table_name, column_list, long_header_lookup):
   # return a list of [column_name, table_name] lists
-  return map(lambda column_name: [column_name, table_name], column_list)
+  return map(lambda column_name: [long_header_lookup.get_value(column_name, 'Long'), table_name], column_list)
 
 def read_data_for_geo_level_into_database(directory, geo_levels, disk_engine):
   table_names = get_table_names_from_database(disk_engine)
@@ -46,23 +47,25 @@ def read_data_for_geo_level_into_database(directory, geo_levels, disk_engine):
     bar = progressbar.ProgressBar(maxval=len(directory_file_list), \
       widgets=[progressbar.Bar('=', '[', ']'), ' ', progressbar.Percentage()])
 
+    long_header_lookup = pandas.read_excel(directory+'/../Metadata/Metadata_2016_GCP_DataPack.xls', sheetname='Cell descriptors information', header=10, index_col=1)
+    long_header_lookup = long_header_lookup[~long_header_lookup.index.duplicated(keep='first')]
+
     bar.start()
     for i, filename in enumerate(directory_file_list):
       if filename == '.DS_Store':
        continue
       bar.update(i)
       reg = r'([A-Z]+)(\d+)([A-Z]*)'
-      # print filename
       match = re.search(reg, filename).group(0)
       table_name = (geo_level + '_' + match)
       if table_name not in table_names:
-        df = pandas.DataFrame.from_csv(data_directory + filename, index_col='region_id')
+        df = pandas.DataFrame.from_csv(data_directory + filename)
         df.to_sql(table_name, disk_engine, if_exists='fail')
         column_list = list(df)
       else:
         column_list = get_column_names_from_table(disk_engine, table_name)
 
-      table_columns_df = table_columns_df.append(pandas.DataFrame(get_column_list_table_name_array(table_name, column_list), columns = ['column', 'table_name']))
+      table_columns_df = table_columns_df.append(pandas.DataFrame(get_column_list_table_name_array(table_name, column_list[1:], long_header_lookup), columns = ['column', 'table_name']))
     bar.finish()
 
     table_columns_df.set_index('column').to_sql('metadata', disk_engine, if_exists='replace')
